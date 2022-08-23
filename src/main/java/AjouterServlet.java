4259250f716111c176837220d33b0b5d05bcd09e
Mauricefhebert <filiatreaultmaurice@gmail.com>
2022-08-23 11:00:40 -0400

    Add some commente to AjouterServlet

--- src/main/java/AjouterServlet.java
+++ src/main/java/AjouterServlet.java
@@ -12,13 +12,24 @@ public class AjouterServlet extends HttpServlet {
     private ProduitDataContext dc = new ProduitDataContext();
 
     @Override
+    /**
+     * utiliser l’objet "request" pour lire l’en-tête HTTP (e.g. cookies) et les
+     * données d’un formulaire HTML
+     */
+    /**
+     * utiliser l’objet "response" pour spécifier l’en-tête de la réponse HTTP e.g.
+     * specifier le content type, et les cookies).
+     */
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        // Recuper le dernier id et ajoute 1 a l'id
         int nextNum = dc.getListeProduits().stream().max(
                 (e1, e2) -> Integer.compare(e1.getNumero(), e2.getNumero()))
                 .get().getNumero() + 1;
         request.setAttribute("NetNum", nextNum);
+        // Recuper la vue
         RequestDispatcher disp = request.getRequestDispatcher("/AjouterProduit.jsp");
+        // Retourn la vue
         disp.forward(request, response); // return View(…)
 
     }
